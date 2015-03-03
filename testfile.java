6d652dd28569d5b5ae655358b124268cc829de1c
Citerius <Johanhej0000@gmail.com>
2015-03-03 23:09:17 +0100

    Update testfile.java

--- testfile.java
+++ testfile.java
@@ -4,7 +4,7 @@ public class CesarCrypt {
 
 	public static void main(String [] args)
 	{	
-		//Hej KARL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//HEJ JOHAN!!!
 		String sentence;
 
 		Scanner input = new Scanner(System.in);
